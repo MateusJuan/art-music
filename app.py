d503f57a58b8f23ea6dc4fe973e14bb1f73c68a7
WhitexNinja <152277272+WhitexNinja@users.noreply.github.com>
2024-11-22 11:49:37 -0300

    22/11/2024

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template, redirect, url_for, request, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import os
 
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "armazenamento.db"))
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+app.config['SECRET_KEY'] = 'chave'
+db = SQLAlchemy(app)
+
+class Usuario(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nickname = db.Column(db.String(150), unique=True, nullable=False)
+    senha = db.Column(db.String(150), nullable=False)
 
