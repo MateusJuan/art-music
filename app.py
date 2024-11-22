f2ac458ab338be585535c9fc5ada6f8d4f35bf19
WhitexNinja <152277272+WhitexNinja@users.noreply.github.com>
2024-11-22 11:18:01 -0300

    criacao_flask

---
+++ app.py
@@ -0,0 +1,5 @@
+from flask import Flask, render_template, redirect, url_for, request, flash, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+
