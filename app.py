8f2f9d8ea510a5df45d6e181170ce90332daa4e0
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-11-24 16:02:27 -0300

    24/11/2024

--- app.py
+++ app.py
@@ -1,36 +1,72 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:Mcmidori3208@localhost/art_music'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
-def init_db():
-    with app.app_context():
-        sql = """
-        CREATE DATABASE IF NOT EXISTS art_music;
-        USE art_music;
-        CREATE TABLE IF NOT EXISTS users (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            username VARCHAR(100) NOT NULL,
-            email VARCHAR(100) NOT NULL,
-            password VARCHAR(255) NOT NULL
-        );
-        """
-        db.session.execute(text(sql))
-        db.session.commit()
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    dados = {
+        'tipos': ['Todos', 'Gospel/Religioso', 'Rock', 'Pagode', 'Samba', 'Clássica'],
+        'conteudos': {
+            'gospel': [
+                {'banda': 'Vocal Livre', 'qtd': 10},
+                {'banda': 'Novo Tom', 'qtd': 15}
+            ],
+            'rock': [
+                {'banda': 'AC/DC', 'qtd': 20},
+                {'banda': 'Queen', 'qtd': 18}
+            ]
+        },
+        'nome': 'Usuário Logado' 
+    }
+    return render_template('index.html', dados=dados)
+
+@app.route('/cifras')
+def cifras():
+    cifras_por_genero = {
+        'Rock': [
+            {'banda': 'AC/DC', 'quantidade': 20},
+            {'banda': 'Queen', 'quantidade': 18}
+        ],
+        'Gospel': [
+            {'banda': 'Vocal Livre', 'quantidade': 10},
+            {'banda': 'Novo Tom', 'quantidade': 15}
+        ]
+    }
+    return render_template('cifras.html', cifras_por_genero=cifras_por_genero)
 
 @app.route('/criarconta')
-def criar_conta():
+def criarconta():
     return render_template('criarconta.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/gospel')
+def gospel():
+    dados = {
+        'tipos': ['Todos', 'Gospel/Religioso', 'Rock', 'Pagode', 'Samba', 'Clássica'],
+        'conteudos': {
+            'gospel': [
+                {'banda': 'Vocal Livre', 'qtd': 10},
+                {'banda': 'Novo Tom', 'qtd': 15}
+            ]
+        }
+    }
+    return render_template('gospel.html', dados=dados)
+
+@app.route('/rock')
+def rock():
+    dados = {
+        'tipos': ['Todos', 'Gospel/Religioso', 'Rock', 'Pagode', 'Samba', 'Clássica'],
+        'conteudos': {
+            'rock': [
+                {'banda': 'AC/DC', 'qtd': 20},
+                {'banda': 'Queen', 'qtd': 18}
+            ]
+        }
+    }
+    return render_template('rock.html', dados=dados)
+
 if __name__ == '__main__':
-    init_db()  
     app.run(debug=True)
