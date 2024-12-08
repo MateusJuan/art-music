ee3b180690827eb6206d763fc03b5b910f1fb380
Mateus juan <169060996+MateusJuan@users.noreply.github.com>
2024-12-07 23:09:10 -0300

    07/12/2024

--- app.py
+++ app.py
@@ -3,50 +3,66 @@ import os
 from datetime import timedelta
 from supabase import create_client, Client
 from werkzeug.security import check_password_hash, generate_password_hash
-from werkzeug.utils import secure_filename
+from dotenv import load_dotenv
+
+# Carregar variáveis de ambiente de um arquivo .env
+load_dotenv()
 
 app = Flask(__name__, static_folder='static')
 app.secret_key = os.urandom(24)
 
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=7)
-UPLOAD_FOLDER = 'static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+# Carregar as variáveis de ambiente
 url = os.getenv("SUPABASE_URL", "https://zhuyytyhkmahjohqbsqd.supabase.co")
 key = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpodXl5dHloa21haGpvaHFic3FkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI4Nzg4NTMsImV4cCI6MjA0ODQ1NDg1M30.cyD6WqNNuGI4kPhtYSjBJ5TNennRxCnizcTrbRH-ufM")
-
 supabase: Client = create_client(url, key)
 
-def upload_file(file):
-    bucket_name = 'art-music'
-    file_name = file.filename
-    file_path_in_bucket = f"files/{file_name}"
+# Função para verificar se o usuário está logado antes de cada requisição
+@app.before_request
+def verificar_login():
+    # Se o usuário não estiver logado, redireciona para o login, exceto nas rotas 'login' e 'criar_conta'
+    if 'nome' not in session and request.endpoint not in ['login', 'criar_conta']:
+        return redirect(url_for('login'))
 
-    result = supabase.storage.from_(bucket_name).upload(file_path_in_bucket, file)
-    
-    if result.error:
-        return None, result.error
-    else:
-        return file_path_in_bucket, None
+@app.route('/criar_conta', methods=['GET', 'POST'])
+def criar_conta():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        senha = request.form['senha']
+        senha_confirmacao = request.form['senha_confirmacao']
 
-def save_file_path_to_db(file_path_in_bucket):
-    data = {'arquivo_path': file_path_in_bucket}
-    
-    result = supabase.from_('arquivos').insert([data]).execute()
-    
-    if result.error:
-        return result.error
-    return None
+        # Verifique se as senhas coincidem
+        if senha != senha_confirmacao:
+            flash("As senhas não coincidem.", "error")
+            return redirect(url_for('criar_conta'))
 
-def get_signed_url(file_path_in_bucket, expiration_time=604800):
-    bucket_name = 'art-music'
+        # Verifique se o email já existe
+        response = supabase.table('usuarios').select('id').eq('email', email).execute()
+        if response.data:
+            flash("Este email já está registrado.", "error")
+            return redirect(url_for('criar_conta'))
 
-    signed_url_result = supabase.storage.from_(bucket_name).create_signed_url(file_path_in_bucket, expiration_time)
-    
-    if signed_url_result.error:
-        return None, signed_url_result.error
-    return signed_url_result.data['signed_url'], None
+        # Criptografe a senha
+        senha_hash = generate_password_hash(senha)
+
+        # Salve o usuário no banco de dados
+        data = {
+            'nome': nome,
+            'email': email,
+            'senha': senha_hash
+        }
+
+        try:
+            supabase.table('usuarios').insert([data]).execute()
+            flash("Conta criada com sucesso!", "success")
+            return redirect(url_for('login'))
+        except Exception as e:
+            flash(f"Erro ao criar conta: {str(e)}", "error")
+            return redirect(url_for('criar_conta'))
+
+    return render_template('criarconta.html')
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -57,54 +73,60 @@ def upload():
     
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
-    
-    file_path_in_bucket, error = upload_file(file)
-    
-    if error:
-        return jsonify({'error': str(error)}), 500
-    
-    error = save_file_path_to_db(file_path_in_bucket)
 
-    if error:
-        return jsonify({'error': str(error)}), 500
-    
+    # Implementar upload do arquivo aqui...
+
     return jsonify({'message': f'File {file.filename} carregado com sucesso!'}), 200
 
-@app.route('/get_signed_url', methods=['GET'])
-def get_url():
-    file_path_in_bucket = request.args.get('file_path')
-    
-    if not file_path_in_bucket:
-        return jsonify({'error': 'file_path is required'}), 400
-    
-    expiration_time = int(request.args.get('expiration_time', 604800))
-    
-    signed_url, error = get_signed_url(file_path_in_bucket, expiration_time=expiration_time)
-    
-    if error:
-        return jsonify({'error': str(error)}), 500
-    
-    return jsonify({'signed_url': signed_url}), 200
+@app.route('/partituras/<estilo>')
+def partituras_por_estilo(estilo):
+    partituras = buscar_partituras(estilo=estilo)
+    return render_template('partituras.html', partituras=partituras, estilo=estilo)
 
-def extrair_nome_arquivo(url):
-    return url.split("/")[-1]
+@app.route('/inserir_partitura', methods=['GET', 'POST'])
+def inserir_partitura():
+    if 'nome' not in session:
+        return redirect(url_for('login'))
 
-app.jinja_env.filters['nome_arquivo'] = extrair_nome_arquivo
+    if request.method == 'POST':
+        estilo_musical = request.form.get('estilo_musical')
+        file = request.files.get('arquivo_pdf')
 
-def buscar_partituras(estilo=None):
-    query = supabase.table('partituras').select('arquivo_pdf')
-    if estilo and estilo != 'Todos':
-        query = query.eq('estilo_musical', estilo)
-    
-    response = query.execute()
-    return response.data if response.data else []
+        if not file:
+            flash("Nenhum arquivo selecionado.", "error")
+            return redirect(url_for('inserir_partitura'))
+
+        if not estilo_musical:
+            flash("O estilo musical é obrigatório.", "error")
+            return redirect(url_for('inserir_partitura'))
+
+        # Realiza o upload do arquivo para o Supabase Storage
+        bucket = supabase.storage.from_('art-music')
+        file_path_in_bucket = f"partituras/{file.filename}"
+
+        try:
+            bucket.upload(file_path_in_bucket, file)
+            # Salvar o caminho do arquivo e o estilo musical no banco de dados
+            data = {
+                'arquivo_pdf': file_path_in_bucket,
+                'estilo_musical': estilo_musical
+            }
+
+            response = supabase.table('partituras').insert([data]).execute()
+            flash("Partitura inserida com sucesso!", "success")
+        except Exception as e:
+            flash(f"Erro ao inserir partitura: {str(e)}", "error")
+
+        return redirect(url_for('home'))
+
+    return render_template('inserir_partitura.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
         senha = request.form['senha']
-        response = supabase.table('usuarios').select('id', 'email', 'senha', 'nome', 'foto_perfil').eq('email', email).execute()
+        response = supabase.table('usuarios').select('id', 'email', 'senha', 'nome').eq('email', email).execute()
 
         if response.data:
             usuario = response.data[0]
@@ -112,7 +134,6 @@ def login():
                 session['nome'] = usuario['nome']
                 session['usuario_id'] = usuario['id']
                 session['email'] = usuario['email']
-                session['foto_perfil'] = usuario.get('foto_perfil', None)
                 return redirect(url_for('home'))
             else:
                 flash("Senha incorreta.", "error")
@@ -127,11 +148,10 @@ def home():
         return redirect(url_for('login'))
 
     nome = session['nome']
-    foto_perfil = session.get('foto_perfil', None)
 
-    partituras = buscar_partituras()
+    partituras = buscar_partituras()  # Agora retornando partituras com URLs assinadas
 
-    return render_template('index.html', nome=nome, foto_perfil=foto_perfil, partituras=partituras)
+    return render_template('index.html', nome=nome, partituras=partituras)
 
 @app.route('/trocarSenha', methods=['GET', 'POST'])
 def trocarSenha():
@@ -159,6 +179,10 @@ def trocarSenha():
             flash("A nova senha e a confirmação não coincidem.", "error")
             return redirect(url_for('trocarSenha'))
 
+        if not is_password_strong(nova_senha):
+            flash("A nova senha não atende aos critérios de segurança.", "error")
+            return redirect(url_for('trocarSenha'))
+
         nova_senha_hash = generate_password_hash(nova_senha, method='pbkdf2:sha256')
         supabase.table('usuarios').update({'senha': nova_senha_hash}).eq('id', session['usuario_id']).execute()
 
@@ -170,6 +194,7 @@ def trocarSenha():
 @app.route('/logout')
 def logout():
     session.clear()
+    flash("Você foi desconectado com sucesso.", "success")
     return redirect(url_for('login'))
 
 @app.route('/perfil')
@@ -179,114 +204,40 @@ def perfil():
 
     nome = session['nome']
     email = session['email']
-    foto_perfil = session.get('foto_perfil', None)
     usuario_id = session['usuario_id'] 
-    return render_template('perfil.html', nome=nome, foto_perfil=foto_perfil, email=email, id=usuario_id)
+    return render_template('perfil.html', nome=nome, email=email, id=usuario_id)
 
-@app.route('/upload_foto', methods=['POST'])
-def upload_foto():
-    if 'nome' not in session:
-        return redirect(url_for('login'))
+def is_password_strong(password):
+    return bool(re.match(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$', password))
 
-    foto = request.files.get('foto_perfil')
+def buscar_partituras(estilo=None):
+    try:
+        query = supabase.table('partituras').select('arquivo_pdf')
+        if estilo and estilo != 'Todos':
+            query = query.eq('estilo_musical', estilo)
+        
+        response = query.execute()
+        partituras = response.data if response.data else []
 
-    if foto:
-        filename = secure_filename(foto.filename)
-        foto_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        foto.save(foto_path)
-
-        session['foto_perfil'] = f'uploads/{filename}'
-
-        supabase.table('usuarios').update({'foto_perfil': f'uploads/{filename}'}).eq('id', session['usuario_id']).execute()
-
-        flash("Foto de perfil atualizada com sucesso!", "success")
-        return redirect(url_for('perfil'))
-
-    flash("Falha ao enviar a foto.", "error")
-    return redirect(url_for('perfil'))
-
-@app.route('/criar_conta', methods=['GET', 'POST'])
-def criar_conta():
-    if request.method == 'POST':
-        nome = request.form.get('nome')
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        if not nome or not email or not password:
-            flash('Todos os campos são obrigatórios!', 'error')
-            return render_template('criarconta.html')
-
-        response = supabase.table('usuarios').select('id').eq('email', email).execute()
-        if response.data:
-            flash('Email já cadastrado!', 'error')
-            return render_template('criarconta.html')
-
-        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
-
-        new_user = {
-            'nome': nome,
-            'email': email,
-            'senha': hashed_password
-        }
-        response = supabase.table('usuarios').insert(new_user).execute()
-
-        if response.error:
-            flash(f'Erro ao criar conta. Tente novamente. {response.error.message}', 'error')
-        else:
-            flash('Conta criada com sucesso!', 'success')
-            return redirect(url_for('login'))
-
-    return render_template('criarconta.html')
-
-
-@app.route('/partituras')
-def partituras():
-    partituras = Partitura.query.all()
-
-    return render_template('partituras.html', partituras=partituras)
-
-
-@app.route('/partituras/estilo/<estilo>') 
-def partituras_por_estilo(estilo):
-    partituras = buscar_partituras(estilo)
-    return jsonify(partituras)
-
-@app.route('/inserir_partitura', methods=['GET', 'POST'])
-def inserir_partitura():
-    if 'nome' not in session:
-        return redirect(url_for('login'))
-
-    if request.method == 'POST':
-        estilo_musical = request.form['estilo_musical']
-        arquivo_pdf = request.files['arquivo_pdf']
-
-        if arquivo_pdf:
-            if not os.path.exists(app.config['UPLOAD_FOLDER']):
-                os.makedirs(app.config['UPLOAD_FOLDER'])
-
-            filename = secure_filename(arquivo_pdf.filename)
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            arquivo_pdf.save(file_path)
-
-            file_url = url_for('static', filename=f'PDF_PARTITURAS/{filename}')
-            
-            new_partitura = {
-                'estilo_musical': estilo_musical,
-                'arquivo_pdf': file_url
-            }
-
-            response = supabase.table('partituras').insert(new_partitura).execute()
-
-            if response.data and len(response.data) > 0:
-                flash('Partitura inserida com sucesso!', 'success')
-                return redirect(url_for('home'))
+        for partitura in partituras:
+            arquivo_url = partitura['arquivo_pdf']
+            signed_url, error = get_signed_url(arquivo_url)
+            if signed_url:
+                partitura['arquivo_pdf'] = signed_url
             else:
-                flash(f"Erro ao inserir partitura: {response.error_message or 'Erro desconhecido'}", 'error')
+                partitura['arquivo_pdf'] = None  # Em caso de erro ao gerar a URL assinada
+        return partituras
+    except Exception as e:
+        return {'error': str(e)}
 
-        else:
-            flash('Nenhum arquivo foi enviado.', 'error')
+def get_signed_url(file_path_in_bucket, expiration_time=604800):
+    bucket_name = 'art-music'
 
-    return render_template('inserir_partitura.html')
+    signed_url_result = supabase.storage.from_(bucket_name).create_signed_url(file_path_in_bucket, expiration_time)
+    
+    if signed_url_result.error:
+        return None, signed_url_result.error
+    return signed_url_result.data['signed_url'], None
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
